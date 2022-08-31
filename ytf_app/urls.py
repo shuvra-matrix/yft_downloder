3a3d3083dead1131a414a02ee935d9e040ab2ed1
SHUVRA CHAKRABARTY <shuvrachakrabarty97@gmail.com>
2022-08-31 09:15:13 +0530

    ytvideo download 720p 360p

--- ytf_app/urls.py
+++ ytf_app/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from ytf_app import viewsaa
+from ytf_app import views
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -8,10 +8,10 @@ app_name = 'ytf'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', viewsaa.index, name='index'),
-    path('ytdown', viewsaa.ydown , name='ytdown'),
-    path('ytdownload',viewsaa.ytdownload, name = 'ytdownload'),
-    path('downloadyt',viewsaa.yvdown,name='yvdown'),
+    path('', views.index, name='index'),
+    path('ytdown', views.ydown , name='ytdown'),
+    path('ytdownload',views.ytdownload, name = 'ytdownload'),
+    path('downloadyt',views.yvdown,name='yvdown'),
 ]
 
 
--- ytf_app/views.py
+++ ytf_app/views.py
@@ -1,3 +1,4 @@
+from hashlib import new
 from django.shortcuts import render,redirect
 from pytube import YouTube
 from django.core.files.storage import FileSystemStorage
@@ -16,38 +17,13 @@ def ydown(request):
         'urls': None,
         'color': 'ytclass',
     }
-    if request.method == 'POST':
-        link = request.POST.get('link')
-        SAVE_PATH = "./media"  
-        try:
-            yt = YouTube(link)
-            print('Title: ',yt.title)
-            #Length of the video
-            print('Length of video: ',yt.length,'seconds')
-        except:
-            print("Connection Error")  # to handle exception
-        d_video = yt.streams.all()
-        print(d_video[1])
-        try:
-            dc = d_video[1].download(SAVE_PATH)
-        except:
-            print("Some Error!")
-
-        print('Task Completed!')
-        url = os.path.basename(dc)
-        my_dict = {
-            'urls': url,
-        }
-
     return render(request, 'ydown.html', context=my_dict)
 
 def ytdownload(request):
     if request.method == 'POST':
-        quality_list = []
-        link_list = []
+        quality_list =[]
         size_list = []
-        link = request.POST.get('link')
-        SAVE_PATH = "./media"  
+        link = request.POST.get('link')  
         try:
             yt = YouTube(link)
             title = yt.title
@@ -58,60 +34,60 @@ def ytdownload(request):
             print("Connection Error")  # to handle exception
         
         try:
-            q360 = yt.streams.filter(res='360p')
-            s1 = yt.streams.get_by_resolution('360p').filesize
+            
+            s1 = (yt.streams.get_by_resolution('360p').filesize)/1000000
+            
+            size_list.append(s1)
             quality_list.append('360p')
-            link_list.append(q360)
-            size_list.append(s1)   
         except:
             pass
         try:
-            q480 = yt.streams.filter(res='480p')
-            quality_list.append('480p')
-            link_list.append(q480)
-            s2 = yt.streams.get_by_resolution('480p').filesize
+            s2 = (yt.streams.get_by_resolution('720p').filesize)/1000000
             size_list.append(s2)
-        except:
-            pass
-        try:
-            q720 = yt.streams.filter(res='720p')
             quality_list.append('720p')
-            link_list.append(q720)
-            s3 = yt.streams.get_by_resolution('720p').filesize
-            size_list.append(s3)
         except:
             pass
-        try:
-            q1080 = yt.streams.filter(res='1080p')
-            quality_list.append('1080p')
-            link_list.append(q1080)
-            s4 = yt.streams.get_by_resolution('1080p').filesize
-            size_list.append(s4)
-        except:
-            pass
-    
-        # print(d_video[1])
-        # try:
-        #     dc = d_video[1].download(SAVE_PATH)
-        # except:
-        #     print("Some Error!")
 
-        # print('Task Completed!')
-        # url = os.path.basename(dc)
-       
-        print(size_list)
-        ranges = range(len(quality_list))
-        print(ranges)
         my_dict = {
             'qlist': quality_list,
-            'llist': link_list,
+            'llist': link,
             'color' : 'ytdown',
             'title' : title,
             'length' :length,
             'thumb' : thumb,
             'size' : size_list,
-            'ranges' : [1,2,3],
         }
-
+        
         return render(request, 'ytdownload.html', context=my_dict)
     return redirect('/')
+
+def yvdown(request):
+    if request.method == 'POST':
+        SAVE_PATH = "./media"
+        dc = None
+        title = request.POST.get('title')
+        link = request.POST.get('link')      
+        reg = request.POST.get('reg')
+        print(reg)
+        thumb = request.POST.get('thumb')
+        try:
+            yt = YouTube(link)
+            link = yt.streams.filter(res=reg,progressive=True).first()
+            print(link)
+            dc = link.download(SAVE_PATH)
+        except:
+            print("Some Error!")
+
+        print('Task Completed!')
+        url = os.path.basename(dc)
+        mydict = {
+    
+            'color' : 'ytdowns',
+            'title' : title,
+            'url' :url,
+            'thumb' : thumb,
+            'reg' :reg,
+        }
+        return render(request,'ytdownpage.html',context=mydict)
+    return redirect('/')
+
