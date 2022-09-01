c3124ca1c250934d9c2b1d27691aaaf53692a333
SHUVRA CHAKRABARTY <75852937+shuvra-matrix@users.noreply.github.com>
2022-09-02 02:56:22 +0530

    complete pwa

--- yft_downloder/settings.py
+++ yft_downloder/settings.py
@@ -207,6 +207,17 @@ PWA_APP_DIR = 'ltr'
 
 PWA_APP_LANG = 'en-US'
 
+PWA_APP = """if ("serviceWorker" in navigator) {
+	window.addEventListener("load", () => {
+		navigator.serviceWorker
+		.register("/sw.js")
+		.then(res => console.log("service worker registered!"))
+		.catch(err => console.log("Your browser support service worker but service worker not registered.", err));
+	});
+} else {
+	console.log(`Your browser Dosn't Support serviceWorker, so you can'n install PWA.`);
+};"""
+
 PWA_SW = """const manifest = self.__WB_MANIFEST;
 if (manifest) {
   // do nothing
