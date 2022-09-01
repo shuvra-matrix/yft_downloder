b2a04c2eeec91bb0cab21ab80376b69bad862b62
SHUVRA CHAKRABARTY <75852937+shuvra-matrix@users.noreply.github.com>
2022-09-02 00:13:36 +0530

    add pwa sysytem

--- yft_downloder/settings.py
+++ yft_downloder/settings.py
@@ -122,8 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = STATIC_DIR
-# STATICFILES_DIRS = [STATIC_DIR]
+# STATIC_ROOT = STATIC_DIR
+STATICFILES_DIRS = [STATIC_DIR]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
