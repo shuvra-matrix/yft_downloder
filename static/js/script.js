2c2d51ae73293e01666eb015092aaf3dfbc16047
SHUVRA CHAKRABARTY <75852937+shuvra-matrix@users.noreply.github.com>
2022-09-04 03:36:14 +0530

    minor update

--- static/js/script.js
+++ static/js/script.js
@@ -2,9 +2,6 @@ let click = document.querySelector(".cros");
 
 let warnng = document.querySelector(".warning_main");
 
-console.log("hewl");
-
 click.addEventListener("click", () => {
-  console.log("hiujijihj");
   warnng.classList.add("hidden_message");
 });
