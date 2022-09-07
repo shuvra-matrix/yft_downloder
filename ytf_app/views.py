d93e2303f6dbec01072636bb78b1e35a531598f3
SHUVRA CHAKRABARTY <75852937+shuvra-matrix@users.noreply.github.com>
2022-09-07 10:09:34 +0530

    update

--- ytf_app/views.py
+++ ytf_app/views.py
@@ -315,8 +315,9 @@ def fbsearch(request):
                 urls = new_url.replace('www', 'mobile')
                 print(urls)
                 req = requ.urlopen(urls)
-                print(req)
-                supe = BeautifulSoup(req, 'html5lib')
+                a = req.read()
+                print(a)
+                supe = BeautifulSoup(a, 'html5lib')
                 print(supe)
                 try:
                     desc = supe.find(
