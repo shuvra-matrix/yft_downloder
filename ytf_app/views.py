625cd211d4b739502246efd25b7ba9ae1547f76a
SHUVRA CHAKRABARTY <75852937+shuvra-matrix@users.noreply.github.com>
2022-09-07 20:03:24 +0530

    update databsae

--- ytf_app/views.py
+++ ytf_app/views.py
@@ -32,7 +32,7 @@ def cloud_upload(dc, fileid):
     url = upload_result['url']
     dir = 'media'
 
-    urls = f'media\\{files}'
+    urls = f'media/{files}'
     filelist = glob.glob(os.path.join(dir, "*"))
     for f in filelist:
         if f == urls:
