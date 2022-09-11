3b6b00cec229b52c392b06b1d343e334b839abae
SHUVRA CHAKRABARTY <75852937+shuvra-matrix@users.noreply.github.com>
2022-09-12 02:59:22 +0530

    update file for deploy

--- ytf_app/views.py
+++ ytf_app/views.py
@@ -32,7 +32,7 @@ def cloud_upload(dc, fileid):
     url = upload_result['url']
     dir = 'media'
 
-    urls = f'media\\{files}'
+    urls = f'media/{files}'
     filelist = glob.glob(os.path.join(dir, "*"))
     print('---------------My Files----------------', url)
     for f in filelist:
