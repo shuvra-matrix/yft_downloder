f38a9bf20f0aa72002eefdac6d2b423a377b0f54
SHUVRA CHAKRABARTY <75852937+shuvra-matrix@users.noreply.github.com>
2022-09-13 21:08:55 +0530

    update database

--- yft_downloder/settings.py
+++ yft_downloder/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-!@assc!vx+b81aypnm!60r+7dylfa2fh=#d!!5)8p3r((2k5j(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ytfdown.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
